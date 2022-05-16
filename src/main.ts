5b84545d8aee27493df2e4b31a5c2e7b15bebbce
Glitch (picturesque-cloud-balloon) <none>
2022-05-16 14:10:15 +0000

    ✉️🔽 Updated with Glitch

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
-import QrScanner from "qr-scanner";
+// import QrScanner from "qr-scanner";
 import "./styles/styles.css";
-const videoElem = document.querySelector("video");
-let searching = false;
+import { setDefaults, toast } from 'bulma-toast'
+
+setDefaults({
+  duration: 2000,
+  position: 'top-right',
+  dismissible: true,
+  animate: { in: 'fadeIn', out: 'fadeOut' }
+})
+// const videoElem = document.querySelector("video");
+// let searching = false;
 
 async function fetchPeople(method: "GET" | "POST" | string, data?: any) {
   const help = await fetch(
@@ -15,36 +23,36 @@ async function fetchPeople(method: "GET" | "POST" | string, data?: any) {
   return await help.json();
 }
 
-const qrScanner = new QrScanner(
-  videoElem,
-  ({ data }) => {
-    if (searching) return;
-    const d = JSON.parse(data);
-    qrScanner.stop();
-    searching = true;
-    fetchPeople("POST", d)
-      .then((res) => {
-        document.getElementById("result-modal").classList.add("is-active");
-        document.getElementById("result").innerHTML = res.ok
-          ? "Processed registration for " + data
-          : "An error occurred: " + JSON.stringify(res.message);
-        if (res.error) console.error(res);
-      })
-      .catch((err) => {
-        document.getElementById("result-modal").classList.add("is-active");
-        document.getElementById("result").innerHTML = JSON.stringify(err);
-        console.error(err);
-      })
-      .finally(() => (searching = false));
-  },
-  {
-    highlightScanRegion: true,
-    highlightCodeOutline: true,
-  }
-);
-document.getElementById("startScan").onclick = () => {
-  qrScanner.start();
-};
+// const qrScanner = new QrScanner(
+//   videoElem,
+//   ({ data }) => {
+//     if (searching) return;
+//     const d = JSON.parse(data);
+//     qrScanner.stop();
+//     searching = true;
+//     fetchPeople("POST", d)
+//       .then((res) => {
+//         document.getElementById("result-modal").classList.add("is-active");
+//         document.getElementById("result").innerHTML = res.ok
+//           ? "Processed registration for " + data
+//           : "An error occurred: " + JSON.stringify(res.message);
+//         if (res.error) console.error(res);
+//       })
+//       .catch((err) => {
+//         document.getElementById("result-modal").classList.add("is-active");
+//         document.getElementById("result").innerHTML = JSON.stringify(err);
+//         console.error(err);
+//       })
+//       .finally(() => (searching = false));
+//   },
+//   {
+//     highlightScanRegion: true,
+//     highlightCodeOutline: true,
+//   }
+// );
+// document.getElementById("startScan").onclick = () => {
+//   qrScanner.start();
+// };
 
 Array.from(
   document.querySelectorAll(".modal-close, .modal-background")
@@ -52,7 +60,7 @@ Array.from(
   ($el) =>
     ($el.onclick = (e) => {
       e.target.parentElement.classList.remove("is-active");
-      qrScanner.start();
+      // qrScanner.start();
     })
 );
 
@@ -86,20 +94,44 @@ fetchPeople("GET").then((people) => {
         name: personName.value,
       };
       console.log(data);
+      personName.parentElement.classList.add("is-loading");
+      personName.disabled = true;
+      document.getElementById("submit").disabled = true;
       fetchPeople("POST", data)
         .then((res: { ok: boolean }) => {
-          document.getElementById("result-modal").classList.add("is-active");
-          document.getElementById("result").innerHTML = res.ok
+          if (res.error) {
+            switch (res.code) {
+              case 1:
+                toast({
+            message: res.message,
+                  duration: 4000,
+            type: 'is-warning',            
+          })
+                break;
+              default: 
+                toast({
+            message: "An error occurred: " + JSON.stringify(res.message),
+            type: 'is-danger',            
+          })
+            }
+            return;
+          }
+          toast({
+            message: res.ok
             ? `Processed registration of Day ${data.day} for ${data.name}`
-            : "An error occurred: " + JSON.stringify(res.message);
-          if (res.error) console.error(res);
+            : "An error occurred: " + JSON.stringify(res.message),
+            type: res.ok ? 'is-success' : 'is-danger',            
+          })
+          
         })
         .catch((err) => {
-          document.getElementById("result-modal").classList.add("is-active");
-          document.getElementById("result").innerHTML = JSON.stringify(err);
+          toast({
+            message: JSON.stringify(err),
+            type: 'is-danger',
+          })
           console.error(err);
         })
-        .finally(() => (personName.value = ""));
+        .finally(() => (personName.value = "", personName.disabled=false, document.getElementById("submit").disabled = false, personName.parentElement.classList.remove("is-loading")));
     }
   });
 
